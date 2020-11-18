a9155e43c75f502f6dfcab2498f44b5961ce6fee
deramat <deramat@student.chalmers.se>
2020-11-18 13:31:03 +0100

    Create publisher file
    Adapted publisher file from Gitlab user dobslaw.

---
+++ src/main/java/Publisher.java
@@ -0,0 +1,38 @@
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+
+public class Publisher {
+
+    private final static String TOPIC = "test";
+
+    private final static String BROKER = "tcp://localhost:1883";
+
+    private final static String USER_ID = "test-publisher";
+
+    private final IMqttClient middleware;
+
+    public Publisher() throws MqttException {
+        middleware = new MqttClient(BROKER, USER_ID);
+        middleware.connect();
+    }
+
+    public static void main(String[] args) throws MqttException {
+        Publisher p = new Publisher();
+        p.sendMessage();
+        p.close();
+    }
+
+    private void close() throws MqttException {
+        middleware.disconnect();
+        middleware.close();
+    }
+
+    private void sendMessage() throws MqttPersistenceException, MqttException {
+        MqttMessage message = new MqttMessage();
+        message.setPayload("Hello World!".getBytes());
+        middleware.publish(TOPIC, message);
+    }
+}
