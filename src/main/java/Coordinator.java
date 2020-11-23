07c065a192fa5a6a47a289ffc39e132f53450b94
Shonaigh Douglas <shonaigh@student.chalmers.se>
2020-11-23 15:59:13 +0100

    Add DentistMqtt.iml to gitignore
    Began coding to reestablish connection

--- src/main/java/Coordinator.java
+++ src/main/java/Coordinator.java
@@ -3,17 +3,18 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 public class Coordinator {
 
     public static void main(String[] args) throws MqttException {
-        Subscriber s = new Subscriber();
-        s.subscribeToMessages();
 
-        DataAccessLayer dal = new DataAccessLayer();
-        DentistRegistry messageTest = dal.loadDentistRegistry();
-        Publisher p = new Publisher();
-        p.sendMessage(messageTest);
-        p.close();
+        //try {
+            Subscriber s = new Subscriber();
+            s.subscribeToMessages();
 
-
+            DataAccessLayer dal = new DataAccessLayer();
+            DentistRegistry messageTest = dal.loadDentistRegistry();
+            Publisher p = new Publisher();
+            p.sendMessage(messageTest);
+            p.close();
+        //} catch (Exception e) {
+            ///e.printStackTrace();
+        //}
     }
-
-
 }
