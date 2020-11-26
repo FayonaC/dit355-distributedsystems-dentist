400101f4b898e0f837cef359826a654fce69ed41
fayona <fayona@student.chalmers.se>
2020-11-26 12:10:10 +0100

    Remove dependency
    Me, Jenni and Negin removed an unwanted dependency from the pom file. We also removed our writing test code from the main.

--- src/main/java/Coordinator.java
+++ src/main/java/Coordinator.java
@@ -3,7 +3,7 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 public class Coordinator {
 
     public static void main(String[] args) throws MqttException {
-/*
+
         //try {
             Subscriber s = new Subscriber();
             s.subscribeToMessages();
@@ -16,16 +16,5 @@ public class Coordinator {
         //} catch (Exception e) {
             ///e.printStackTrace();
         //}
-*/
-        DataAccessLayer dal = new DataAccessLayer();
-        DentistRegistry dentistsJSON = dal.loadDentistRegistry();
-        System.out.println("read: " + dentistsJSON);
-
-        Dentist newDentist = new Dentist(1, "name", "owner", 1, "address", "city", 12.3,
-                14.5, "shjgsh", "tjhsg0", "jshgj", "jafh", "gjshg");
-        dentistsJSON.addDentist(newDentist);
-        System.out.println("before write: " + dentistsJSON);
-
-       dal.saveDentists(dentistsJSON);
     }
 }
