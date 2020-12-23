29fefe466c2828cc9ecde54d08f2aa321c9ef61a
deramat <deramat@student.chalmers.se>
2020-12-23 14:36:16 +0100

    Fix unsubscribe
    Dominique and Victoria refactored the connectionLost method to also unsubscribe before disconnecting and closing, a timer is used.
    
    Resolves: #13

--- src/main/java/Subscriber.java
+++ src/main/java/Subscriber.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -42,26 +43,45 @@ public class Subscriber implements MqttCallback {
     @Override
     public void connectionLost(Throwable throwable) {
         System.out.println("Connection lost!");
+        long startTime = System.currentTimeMillis();
+        long elapsedTime = 0;
 
-       while (middleware.isConnected() == false) {
-
-           // Reestablish connection lost
-           try {
+        while (middleware.isConnected() == false && elapsedTime < 60 * 1000) {
+            // reestablish lost connection
+            try {
                 Thread.sleep(3000);
                 System.out.println("Reconnecting..");
                 middleware.reconnect();
+                elapsedTime = (new Date()).getTime() - startTime;
 
-           } catch (Exception e) {
-               throwable.getMessage();
-           }
-       }
-        try {
-            middleware.subscribe("SuccessfulBooking");
-        } catch (MqttException e) {
-            e.printStackTrace();
+            } catch (Exception e) {
+
+            }
         }
-        System.out.println("Connection to broker reestablished!");
+        if (middleware.isConnected() == false) {
+            try {
+                System.out.println("Tried reconnecting for 1 minute, now disconnecting..");
+                middleware.unsubscribe("Dentists");
+                middleware.disconnect();
+                middleware.close();
+                System.out.println("Dentist RIP :(");
+                System.out.println("Please restart broker and component");
+
+            } catch (
+                    MqttException mqttException) {
+                throwable.getMessage();
+            }
+        }
+        if (middleware.isConnected() == true) {
+            try {
+                middleware.subscribe("Dentists");
+                System.out.println("Connection to broker reestablished!");
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken token) {
